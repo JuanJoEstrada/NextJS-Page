a72835ba887a410805015734f146eae2b6d0558b
JuanEstrada <juan_estrada_1@hotmail.com>
2022-02-28 13:14:42 -0600

    Using useState for input

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -1,38 +1,86 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState } from "react"
+import classes from "./auth-form.module.css"
 
-function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+const createUser = async (email: string, password: string) => {
+	const response = await fetch("/api/auth", {
+		method: "POST",
+		body: JSON.stringify({ email, password }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
 
-  function switchAuthModeHandler() {
-    setIsLogin((prevState) => !prevState);
-  }
+	const data = await response.json()
+	if (!response.ok) {
+		throw new Error(data.message || "Something went wrong!")
+	}
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+	return data
 }
 
-export default AuthForm;
+const AuthForm = () => {
+	const [emailInput, setEmailInput] = useState("")
+	const [passwordInput, setPasswordInput] = useState("")
+	const [isLogin, setIsLogin] = useState(true)
+
+	console.log("emailInputRef:", emailInput)
+	console.log("passwordInputRef:", passwordInput)
+
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState)
+	}
+
+	const submitHandler = async (event: { preventDefault: () => void }) => {
+		event.preventDefault()
+		if (isLogin) {
+			// log user in
+		} else {
+			try {
+				const result = await createUser(emailInput, passwordInput)
+				console.log("User create:", result)
+			} catch (error) {
+				console.log("Error creating user:", error)
+			}
+		}
+	}
+
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? "Login" : "Sign Up"}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<label htmlFor='email'>Your Email</label>
+					<input
+						type='email'
+						id='email'
+						value={emailInput}
+						onChange={({ target }) => setEmailInput(target.value)}
+						required
+					/>
+				</div>
+				<div className={classes.control}>
+					<label htmlFor='password'>Your Password</label>
+					<input
+						type='password'
+						id='password'
+						value={passwordInput}
+						onChange={({ target }) => setPasswordInput(target.value)}
+						required
+					/>
+				</div>
+				<div className={classes.actions}>
+					<button>{isLogin ? "Login" : "Create Account"}</button>
+					<button
+						type='button'
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}
+					>
+						{isLogin ? "Create new account" : "Login with existing account"}
+					</button>
+				</div>
+			</form>
+		</section>
+	)
+}
+
+export default AuthForm
