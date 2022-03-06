955c4d80780f5a61823c52840bc0dc20ef55dcc6
JuanEstrada <juan_estrada_1@hotmail.com>
2022-03-05 21:15:49 -0600

    Add types

--- components/second_layout/layout.tsx
+++ components/second_layout/layout.tsx
@@ -1,14 +1,16 @@
-import { Fragment } from 'react';
+import { Fragment, ReactChild, ReactFragment, ReactPortal } from "react"
 
-import MainNavigation from './main-navigation';
+import MainNavigation from "./main-navigation"
 
-function Layout(props) {
-  return (
-    <Fragment>
-      <MainNavigation />
-      <main>{props.children}</main>
-    </Fragment>
-  );
+function Layout(props: {
+	children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined
+}) {
+	return (
+		<Fragment>
+			<MainNavigation />
+			<main>{props.children}</main>
+		</Fragment>
+	)
 }
 
-export default Layout;
+export default Layout
